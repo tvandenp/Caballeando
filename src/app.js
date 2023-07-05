341bd4216e43cb33616c2beb60e6acb5ab784c74
tvandenp <tomas.vanden@gmail.com>
2023-07-05 15:04:57 -0300

    Primer seteo

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// Módulos
+
+const express = require('express');
+const path = require('path');
+const methodOverride =  require('method-override');
+const session = require('express-session');
+const cookies = require('cookie-parser');
+
+// Ejecución
+const app = express();
+
+// Configuración
+const publicPath = path.resolve(__dirname, "../public");
+app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(session({
+	secret: "Secret word",
+	resave: false,
+	saveUninitialized: false,
+}));
+app.use(cookies());
+
+// Motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Rutas
+const mainRoutes = require('./routes/mainRouter');
+app.use('/', mainRoutes);
+
+const userRoutes = require('./routes/usersRouter');
+app.use('/users', userRoutes);
+
+const productsRoutes = require('./routes/productsRouter');
+app.use('/products', productsRoutes);
+
+// Servidor
+app.listen(process.env.PORT || 3030, () => {
+    console.log("Servidor corriendo en el puerto 3030, ¡vamos el Campo!");
+});
---
+++ src/routes/mainRouter.js
@@ -0,0 +1,20 @@
+// Módulos
+const express = require('express');
+
+// Ejecución
+const router = express.Router();
+
+// Controlador
+const mainController = require('../controllers/mainController');
+
+// Index
+router.get('/', mainController.index);
+
+// Carrito de compras
+router.get('/shopping-cart', ifGuestMiddleware, mainController.shoppingCart);
+router.post('/shopping-cart', ifGuestMiddleware, mainController.shoppingCartAdd);
+router.put('/shopping-cart/update', ifGuestMiddleware, mainController.shoppingCartUpdate);
+router.delete('/shopping-cart/delete', ifGuestMiddleware, mainController.shoppingCartDelete);
+
+
+module.exports = router;
